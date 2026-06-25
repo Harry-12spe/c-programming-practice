e5227467c4fce45a01b57b22eea9330f8caef0a6
Hariom Ray <hariomkumarrai696@gmail.com>
2026-06-25 20:12:08 +0530

    DAY20

---
+++ DAY20/Q78_Symmetry_matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+    printf("Enter matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    int sym = 1;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (a[i][j] != a[j][i]) { sym = 0; break; }
+
+    printf(sym ? "Symmetric\n" : "Not Symmetric\n");
+    return 0;
+}
