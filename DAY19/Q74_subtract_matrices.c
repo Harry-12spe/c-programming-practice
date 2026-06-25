e89c4eee2be5ca85b43f0dd129a83de9ebaa8bec
Hariom Ray <hariomkumarrai696@gmail.com>
2026-06-25 19:50:08 +0530

    DAY19

---
+++ DAY19/Q74_subtract_matrices.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter rows and cols: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c], b[r][c], res[r][c];
+
+    printf("Enter matrix A:\n");
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter matrix B:\n");
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &b[i][j]);
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            res[i][j] = a[i][j] - b[i][j];
+
+    printf("Result:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++)
+            printf("%d ", res[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
