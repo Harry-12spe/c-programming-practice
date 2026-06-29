06e08a6dc13c042083eb67e4bcc269845ee443b8
Hariom Ray <hariomkumarrai696@gmail.com>
2026-06-30 00:11:16 +0530

    Add Q101_Create_number_guessing_game.c for DAY26

---
+++ DAY26/Q101_Create_number_guessing_game.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secret, guess;
+    srand(time(NULL));
+    secret = rand() % 100 + 1;
+    printf("Guess the number between 1 and 100\n");
+    do {
+        printf("Enter guess: ");
+        scanf("%d", &guess);
+        if (guess > secret) printf("Too high!\n");
+        else if (guess < secret) printf("Too low!\n");
+        else printf("Correct!\n");
+    } while (guess != secret);
+    return 0;
+}
