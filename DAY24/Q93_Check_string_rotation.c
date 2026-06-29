5c0a204de30d3d6f3b3470ec0dd7c040820a163b
Hariom Ray <hariomkumarrai696@gmail.com>
2026-06-30 00:11:15 +0530

    Add Q93_Check_string_rotation.c for DAY24

---
+++ DAY24/Q93_Check_string_rotation.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[200];
+    printf("Enter string 1: ");
+    scanf("%s", str1);
+    printf("Enter string 2: ");
+    scanf("%s", str2);
+    if (strlen(str1) != strlen(str2)) {
+        printf("Not a rotation\n");
+        return 0;
+    }
+    strcpy(temp, str1);
+    strcat(temp, str1);
+    if (strstr(temp, str2) != NULL) {
+        printf("Rotation\n");
+    } else {
+        printf("Not a rotation\n");
+    }
+    return 0;
+}
