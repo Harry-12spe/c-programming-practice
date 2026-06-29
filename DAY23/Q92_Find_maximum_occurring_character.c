abcd7a11ad7a11919805b09de4360669e0ab19d5
Hariom Ray <hariomkumarrai696@gmail.com>
2026-06-30 00:11:15 +0530

    Add Q92_Find_maximum_occurring_character.c for DAY23

---
+++ DAY23/Q92_Find_maximum_occurring_character.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char maxChar;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    for (i = 0; str[i] != '\0'; i++) {
+        freq[(int)str[i]]++;
+        if (freq[(int)str[i]] > max) {
+            max = freq[(int)str[i]];
+            maxChar = str[i];
+        }
+    }
+    printf("Maximum occurring character: %c\n", maxChar);
+    return 0;
+}
