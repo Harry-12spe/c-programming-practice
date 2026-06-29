e95ceb8505a598e7623767c5e22c72dfc5e862d0
Hariom Ray <hariomkumarrai696@gmail.com>
2026-06-30 00:11:14 +0530

    Add Q86_Count_words_in_a_sentence.c for DAY22

---
+++ DAY22/Q86_Count_words_in_a_sentence.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+    int count = 1, i;
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' && str[i+1] != ' ' && str[i] != '\n') {
+            count++;
+        }
+    }
+    printf("Number of words: %d\n", count);
+    return 0;
+}
